c5c4f210bb89713e2a992f1f60bc68094fbc48b9
Eden Nalian <122690785+EdenNal@users.noreply.github.com>
2025-01-16 11:31:43 -0500

    finished this task (close task #5)

--- mcmerch.py
+++ mcmerch.py
@@ -1,23 +1,18 @@
-import sys
+import argparse
+def main():
+   # configure first cli arg to be the price of the item
+   parser = argparse.ArgumentParser()
+   parser.add_argument("price", type=float, help="The price of the item")
+   args = parser.parse_args()
+   price = args.price
 
-PROVINCE_TAX_RATES = {
-    "ON": 0.13,  # Ontario
-}
 
-if len(sys.argv) != 3:
-    print("Usage: python mcmerch.py <price> <province_code>")
-    sys.exit(1)
+   # add tax to the price
+   total = price * 1.13 # since in Ontario
 
-try:
-    price = float(sys.argv[1])
-    province_code = sys.argv[2].upper()
-    tax_rate = PROVINCE_TAX_RATES[province_code]
 
-except ValueError:
-    print("Please provide a valid number for the price.")
-    sys.exit(1)
+   print(f"The total price is {total:.2f}")
 
-# Calculate total price
-total_price = price * (1 + tax_rate)
 
-print(f"Total price after tax for {province_code}: ${total_price:.2f}")
+if __name__ == '__main__':
+   main()
